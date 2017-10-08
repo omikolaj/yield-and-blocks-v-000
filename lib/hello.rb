657732ac14ae9c8508cb7e34595ef81287275fa2
Oskar Mikolajczyk <omikolaj1@gmail.com>
2017-10-08 11:16:39 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,3 +1,22 @@
+def names_starting_with_o(list)
+    list.each do | o | 
+      if ( o.start_with?("O")
+        yield (o) #At this point in our code, we have the variable
+                  #we were looking for,
+                  #so we will take it, and return to the method block.
+    end
+  end
+end
+
+
+
+
+
+
+
+
+
+
 def hello_t (array)
   if block_given?
     i = 0
